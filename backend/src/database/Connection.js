59aa52bf8bd8f61b6efcd3e5b961dab8dee5bfac
VictorBorgesLoures <victorblp@hotmail.com>
2024-03-31 18:20:02 -0300

    conexao com banco de dados

---
+++ backend/src/database/Connection.js
@@ -0,0 +1,29 @@
+let mysqlPool = require('mysql').createPool({
+    connectionLimit : 10,
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE,
+});
+
+let logger = require('../../logger/Logger');
+
+export class DBConnection {
+
+    constructor() {
+        logger.danger('DBConnection', this);
+        //throw new Error("DBConnection can't be instaciated");
+    }
+
+    static async createPool(query) {
+        return new Promise((resolve, reject) => {
+            mysqlPool.query(query, (err, results) => {
+                if(err) {
+                    logger.danger('DBConnection', 'Error on query: '+ query);
+                    reject(err);
+                } else 
+                    resolve(results);                
+            });
+        });
+    }
+}
