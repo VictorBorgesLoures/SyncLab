9dbadf3ba13ebd9d89a287403d0ab358a5884510
VictorBorgesLoures <victorblp@hotmail.com>
2024-08-18 12:56:15 -0300

    user loader update

--- backend/config/routes/user/loader.js
+++ backend/config/routes/user/loader.js
@@ -1,9 +1,22 @@
+const Logger = require('../../../logger/Logger');
+
 module.exports = app => {
 
     //Check if assessing api, then must be a valid user
     app.use('*', async (req, res, next) => {
         if(req._parsedUrl.pathname == '/api') {
-
+            User.fectchUser(req.sessionStore.sessions[req.sessionId].id)
+            .then(u => {
+                if(u) {
+                    req.user = u;
+                    next();
+                } else {
+                    res.status(500).json({status:500, msg:"Sem autorização"})
+                }
+            })
+            .catch(e => {
+                Logger.danger(e);
+            })
         }
     })
     
