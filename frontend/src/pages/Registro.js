36e9085beec5faefc017e6464f8812d543ce9736
WendellReis <wendellreismatias@gmail.com>
2024-08-20 18:09:11 -0300

    Adição do ícone e título das páginas e criação do arquivos da tela de registro

---
+++ frontend/src/pages/Registro.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import logo from '../assets/logo.png';
+
+function Registro() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Registro;
