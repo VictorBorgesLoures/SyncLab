e96d03cdb034348f5e503cfac84aac5c68917ab9
VictorBorgesLoures <victorblp@hotmail.com>
2024-08-28 10:27:53 -0300

    update app and routes

--- backend/config/routes/user/auth.js
+++ backend/config/routes/user/auth.js
@@ -20,21 +20,21 @@ export default app => {
 
     app.use(auth);
 
-    app.post('/api/auth', (req, res, next) => {
+    app.post('/auth', (req, res, next) => {
         Logger.info("API/AUTH", "AUTH CALL");
         if (req.session.user) {
             Logger.info("API/AUTH", "VALID USER");
             if (req.session.user.matricula) {
                 Logger.info("API/AUTH", "redirect dashbard");
-                res.status(301).json({ status: 301, redirect: "/dashboard", msg: "Sessão Válida" });
+                res.status(301).json({ status: 301, msg: "Sessão Válida" });
             }
             else {
                 Logger.info("API/AUTH", "redirect matricula");
                 res.status(301).json({ status: 301, redirect: "/matricula", msg: "Sessão válida, porém sem matrícula" });
             }
         } else {
-            Logger.info("API/AUTH", "Invalid session, redirect logout");
-            res.status(301).json({ status: 301, redirect:'/logout', msg: "Sessão Inválida" });
+            Logger.info("API/AUTH", "Invalid session, redirect login");
+            res.status(301).json({ status: 301, redirect:'/login', msg: "Sessão Inválida" });
         }
     })
 }
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,17 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, useNavigate } from "react-router-dom";
 import App from './App';
 import Registro from './pages/Registro';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import MatriculaLoign from './pages/MatriculaLogin';
+import MatriculaLogin from './pages/MatriculaLogin';
+
+import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter(
     [
         {
             path: "/",
             element: <App />,
-            loader: rootLoader,
             children: [
                 {
                     path: "login",
@@ -18,16 +19,22 @@ const router = createBrowserRouter(
                 },
                 {
                     path: "registro",
-                    element: <Login />
+                    element: <Registro />
                 },
                 {
-                    path: "dashboard",
-                    element: <Login />
-                },
-                {
-                    path: "matricula",
-                    element: <Login />
-                },
+                    path: "*",
+                    element: <ProtectedRoute></ProtectedRoute>,
+                    children: [
+                        {
+                            path: "dashboard",
+                            element: <Dashboard />
+                        },
+                        {
+                            path: "matricula",
+                            element: <MatriculaLogin />
+                        }
+                    ]
+                }
             ],
         },
     ]
