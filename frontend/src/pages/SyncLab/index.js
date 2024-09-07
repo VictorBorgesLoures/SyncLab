1720feae63e591d9bd70acbd5ec48fae791ae65b
WendellReis <wendellreismatias@gmail.com>
2024-09-07 10:30:12 -0300

    fix import

--- frontend/src/pages/SyncLab/index.js
+++ frontend/src/pages/SyncLab/index.js
@@ -5,7 +5,6 @@ import UserHeader from '../../components/userHeader';
 import Painel from '../../components/painel';
 import withRouter from "../../components/withRouter";
 import './styles.css';
-import withRouter from '../../components/withRouter';
 
 class SyncLab extends Component {
     constructor(props) {
