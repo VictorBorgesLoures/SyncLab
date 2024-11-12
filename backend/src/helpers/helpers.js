1d8fc96142830d5ae00adcb5ce855a663de8302b
VictorBorgesLoures <victorblp@hotmail.com>
2024-11-12 07:17:01 -0300

    regex fix

--- backend/src/helpers/helpers.js
+++ backend/src/helpers/helpers.js
@@ -1,18 +1,17 @@
 let enumTypes = {
-    "id": /\d{1,11}/,
+    "id": /^\d{1,11}$/,
     "email": /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    "rua": /^[\p{L}\p{N}\s]*/,
+    "rua": /^[\p{L}\p{N}\s]*$/,
     "numero": /^\d{1,11}$/,
     "cep": /^\d{5}-?\d{3}$/,
     "complemento": /^[\w\s.,/()[\]-]{1,255}$/,
     "data": /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
     "username": /^[A-Za-z]{1,100}(?:_[A-Za-z]{1,100}){0,99}$/,
-    "password": /\d{1,40}/,
+    "password": /^\d{1,40}$/,
     "nome": /^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+){0,254}$/,
     "matricula": /^\d{9,15}$/,
     "cpf": /^\d{11}$/,
-    "matricula": /^\d{9,15}$/,
-    "matriculaType": /[1-3]/
+    "matriculaType": /^[1-3]$/
 };
 
 export default {
